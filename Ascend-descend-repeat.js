91b6389ef9a0cd760b06d8ca4d57403a9c183af3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-28 19:38:24 -0700

    Update Ascend-descend-repeat.js

--- Ascend-descend-repeat.js
+++ Ascend-descend-repeat.js
@@ -41,3 +41,15 @@ function ascendDescend(length, minimum, maximum) {
 }
 
 // or
+
+function ascendDescend(length, minimum, maximum) {
+  let result = ''
+  for (let i = minimum; i <= maximum; i++) {
+    result += i
+  }
+  for (let i = maximum - 1; i > minimum; i--) {
+    result += i
+  }
+  result = result.repeat(length)
+  return result.substring(0,length)  
+}
